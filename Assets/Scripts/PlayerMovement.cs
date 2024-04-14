2404b460d902d0f2c1d6f4a2c5322a525b081296
Velciu A Razvan-Gabriel <92295676+RazvanV12@users.noreply.github.com>
2024-04-14 13:34:55 +0300

    Player Movement
    Not completed

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float jumpForce = 5f;
+    
+    private Rigidbody2D rb; 
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        var moveInput = Input.GetAxis("Horizontal");
+        transform.position = new Vector2(transform.position.x + moveInput * speed * Time.deltaTime, transform.position.y);
         
+        if (Input.GetKeyDown(KeyCode.W) && Mathf.Abs(rb.velocity.y) < 0.001f)
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+        
+        // Make the player be able to duck by pressing S key - when you duck, you can't move and the height
+        // of the player is reduced to half
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.localScale = new Vector3(1, 0.5f, 1);
+            speed = 0f;
+        }
+        else
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+            speed = 5f;
+        }
     }
 }

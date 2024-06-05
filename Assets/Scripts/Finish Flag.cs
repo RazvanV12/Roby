b59f0439a64d26e277b136cc77e1e01bd8bf472c
RazvanV12 <92295676+RazvanV12@users.noreply.github.com>
2024-06-05 13:58:49 +0300

    Added Level Clear Panel

--- Assets/Scripts/Finish Flag.cs
+++ Assets/Scripts/Finish Flag.cs
@@ -6,13 +6,10 @@ using UnityEngine.SceneManagement;
 
 public class FinishFlag : MonoBehaviour
 {
-    [SerializeField] private GameObject FinishedLevelMenuUI;
     
-    [SerializeField] private AudioManager audioManager;
     [SerializeField] private GameManager gameManager;
     private void Awake()
     {
-        audioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioManager>();
         gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
     }
     // Start is called before the first frame update
@@ -30,30 +27,9 @@ public class FinishFlag : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            UnlockNewLevel();
-            SaveCollectedCoins(gameManager.GetCollectedCoins());
-            audioManager.StopBGM();
-            FinishedLevelMenuUI.SetActive(true);
+            gameManager.FinishedLevel();
             Debug.Log("Player reached the finish line");
         }
     }
     
-    private void UnlockNewLevel()
-    {
-        if(SceneManager.GetActiveScene().buildIndex >= PlayerPrefs.GetInt("ReachedIndex"))
-        {
-            PlayerPrefs.SetInt("ReachedIndex", SceneManager.GetActiveScene().buildIndex + 1);
-            PlayerPrefs.SetInt("UnlockedLevel", PlayerPrefs.GetInt("UnlockedLevel", 1) + 1);
-            PlayerPrefs.Save();
-        }
-    }
-
-    private void SaveCollectedCoins(int collectedCoins)
-    {
-        if(collectedCoins > PlayerPrefs.GetInt("CollectedCoins_Level " + (SceneManager.GetActiveScene().buildIndex - 1), 0))
-        {
-            PlayerPrefs.SetInt("CollectedCoins_Level " + (SceneManager.GetActiveScene().buildIndex - 1), collectedCoins);
-            PlayerPrefs.Save();
-        }
-    }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -13,9 +15,22 @@ public class GameManager : MonoBehaviour
     [SerializeField] private TextMeshProUGUI uiCoins;
 
     [SerializeField] private GameObject DiedMenuUI;
+    [SerializeField] private GameObject FinishedLevelUI;
+    [SerializeField] private GameObject LevelClearPanel;
     [SerializeField] private Transform playerPosition;
 
     [SerializeField] private AudioManager audioManager;
+    [SerializeField] private FinishFlag finishFlag;
+
+    [SerializeField] private Sprite fullStar;
+    [SerializeField] private Sprite emptyStar;
+    
+    private Image star1Image;
+    private Image star2Image;
+    private Image star3Image;
+    private TextMeshProUGUI scoreText;
+    private bool levelEnded = false;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -25,20 +40,83 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        timeSinceGameStarted += Time.deltaTime;
-        uiTimer.text = "Timer: " + timeSinceGameStarted.ToString("F2");
-        coinsCollected = itemCollector.GetCoins();
-        uiCoins.text = "Coins: " + coinsCollected;
+        if (!levelEnded)
+        {
+            timeSinceGameStarted += Time.deltaTime;
+            uiTimer.text = "Timer: " + timeSinceGameStarted.ToString("F2");
+            coinsCollected = itemCollector.GetCoins();
+            uiCoins.text = "Coins: " + coinsCollected;
+        }
+
         if(playerPosition.position.y < -3f)
         {
             DiedMenuUI.SetActive(true);
+            levelEnded = true;
             audioManager.StopBGM();
         }
     }
     
-    // getter for coins collected
-    public int GetCollectedCoins()
+    public void FinishedLevel()
     {
-        return coinsCollected;
+        levelEnded = true;
+        UnlockNewLevel();
+        SaveCollectedCoins();
+        ModifyLevelClearPanel();
+        LevelClearPanel.SetActive(true);
+        audioManager.StopBGM();
+    }
+
+    private void ModifyLevelClearPanel()
+    {
+        star1Image = LevelClearPanel.transform.GetChild(2).GetComponent<Image>();
+        star2Image = LevelClearPanel.transform.GetChild(3).GetComponent<Image>();
+        star3Image = LevelClearPanel.transform.GetChild(4).GetComponent<Image>();
+        scoreText = LevelClearPanel.transform.GetChild(5).GetComponent<TextMeshProUGUI>();
+        
+        star1Image.sprite = emptyStar;
+        star2Image.sprite = emptyStar;
+        star3Image.sprite = emptyStar;
+
+        switch (coinsCollected)
+        {
+            case 1:
+                star1Image.sprite = fullStar;
+                break;
+            case 2:
+                star1Image.sprite = fullStar;
+                star2Image.sprite = fullStar;
+                break;
+            case 3:
+                star1Image.sprite = fullStar;
+                star2Image.sprite = fullStar;
+                star3Image.sprite = fullStar;
+                break;
+        }
+
+        scoreText.text = "Score: " + CalculateScore().ToString("F2");
+    }
+
+    private float CalculateScore()
+    {
+        return timeSinceGameStarted - 5 * coinsCollected;
+    }
+    
+    private void UnlockNewLevel()
+    {
+        if(SceneManager.GetActiveScene().buildIndex >= PlayerPrefs.GetInt("ReachedIndex"))
+        {
+            PlayerPrefs.SetInt("ReachedIndex", SceneManager.GetActiveScene().buildIndex + 1);
+            PlayerPrefs.SetInt("UnlockedLevel", PlayerPrefs.GetInt("UnlockedLevel", 1) + 1);
+            PlayerPrefs.Save();
+        }
+    }
+
+    private void SaveCollectedCoins()
+    {
+        if(coinsCollected > PlayerPrefs.GetInt("CollectedCoins_Level " + (SceneManager.GetActiveScene().buildIndex - 1), 0))
+        {
+            PlayerPrefs.SetInt("CollectedCoins_Level " + (SceneManager.GetActiveScene().buildIndex - 1), coinsCollected);
+            PlayerPrefs.Save();
+        }
     }
 }
